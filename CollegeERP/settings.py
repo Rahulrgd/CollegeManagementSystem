2ef14cd56b05415c9682318d362e7805ffa1783a
RahulGupta <rahulrgd21@gmail.com>
2022-10-06 23:39:52 +0530

    steps

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -121,16 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-# LOGIN_REDIRECT_URL = '/'
-
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
+LOGIN_REDIRECT_URL = '/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
